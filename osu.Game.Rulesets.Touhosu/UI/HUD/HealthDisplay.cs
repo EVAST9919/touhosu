10402122f5b8c9a38cfbd5b2a7dd474b839274f4
Andrei Zavatski <megaman9919@gmail.com>
2020-04-19 18:04:45 +0300

    Add UI for HealthDisplay

--- osu.Game.Rulesets.Touhosu/UI/HUD/HealthDisplay.cs
+++ osu.Game.Rulesets.Touhosu/UI/HUD/HealthDisplay.cs
@@ -1,22 +1,41 @@
 ﻿using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics;
-using osu.Game.Graphics.Sprites;
 using osu.Game.Rulesets.Touhosu.Scoring;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics.Sprites;
+using osuTK;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.IEnumerableExtensions;
 
 namespace osu.Game.Rulesets.Touhosu.UI.HUD
 {
     public class HealthDisplay : CompositeDrawable
     {
-        private readonly OsuSpriteText text;
         private readonly TouhosuHealthProcessor healthProcessor;
+
+        [Resolved]
+        private TextureStore textures { get; set; }
+
+        private readonly Sprite[] hearts = new Sprite[10];
 
         public HealthDisplay(TouhosuHealthProcessor healthProcessor)
         {
             this.healthProcessor = healthProcessor;
 
+            FillFlowContainer flow;
+
             AutoSizeAxes = Axes.Both;
-            AddInternal(text = new OsuSpriteText());
+            AddInternal(flow = new FillFlowContainer
+            {
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal
+            });
+
+            for (int i = 0; i < 10; i++)
+                hearts[i] = new Sprite { Size = new Vector2(8) };
+
+            hearts.ForEach(flow.Add);
         }
 
         protected override void LoadComplete()
@@ -28,7 +47,32 @@ namespace osu.Game.Rulesets.Touhosu.UI.HUD
 
         private void onHealthChanged(ValueChangedEvent<double> health)
         {
-            text.Text = health.NewValue.ToString();
+            for (int i = 0; i < 10; i++)
+            {
+                if (health.NewValue == 1)
+                    hearts[i].Texture = textures.Get("HUD/HP/full");
+                else
+                    hearts[i].Texture = getTexture(health.NewValue, i);
+            }
+        }
+
+        private Texture getTexture(double value, int index)
+        {
+            var heartHealth = value * 10 - index;
+
+            if (heartHealth > 1)
+                return textures.Get("HUD/HP/full");
+
+            if (heartHealth > 0.75f)
+                return textures.Get("HUD/HP/75");
+
+            if (heartHealth > 0.5f)
+                return textures.Get("HUD/HP/50");
+
+            if (heartHealth > 0.25f)
+                return textures.Get("HUD/HP/25");
+
+            return textures.Get("HUD/HP/0");
         }
     }
 }
