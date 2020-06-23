9af6283c5f327188b0f62aeae9d056f978272b7e
Andrei Zavatski <megaman9919@gmail.com>
2020-06-23 18:03:44 +0300

    Added player test scene

---
+++ osu.Game.Rulesets.Touhosu.Tests/TestSceneTouhosuPlayer.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Touhosu.Tests
+{
+    [TestFixture]
+    public class TestSceneTouhosuPlayer : PlayerTestScene
+    {
+        protected override Ruleset CreatePlayerRuleset() => new TouhosuRuleset();
+    }
+}
