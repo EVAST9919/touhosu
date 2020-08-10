71987c594efbbd74c71aa8fed5e84cf659cdb5bd
Andrei Zavatski <megaman9919@gmail.com>
2020-08-10 21:45:51 +0300

    Remove not used stuff

--- osu.Game.Rulesets.Touhosu/Extensions/ProjectileExtensions.cs
+++ osu.Game.Rulesets.Touhosu/Extensions/ProjectileExtensions.cs
@@ -15,13 +15,6 @@ namespace osu.Game.Rulesets.Touhosu.Extensions
 {
     public static class ProjectileExtensions
     {
-        private const int bullets_per_hitcircle = 5;
-        private const int hitcircle_angle_offset = 5;
-
-        private const int bullets_per_spinner_span = 20;
-        private const float spinner_span_delay = 250f;
-        private const float spinner_angle_per_span = 8f;
-
         public static List<TouhosuHitObject> ConvertSlider(HitObject obj, IBeatmap beatmap, IHasPathWithRepeats curve, int index)
         {
             double spanDuration = curve.Duration / (curve.RepeatCount + 1);
@@ -31,69 +24,6 @@ namespace osu.Game.Rulesets.Touhosu.Extensions
                 return generateBuzzSlider(obj, beatmap, curve, spanDuration, index);
             else
                 return generateDefaultSlider(obj, beatmap, curve, spanDuration, index);
-        }
-
-        public static List<TouhosuHitObject> ConvertHitCircle(HitObject obj, int index, int indexInCurrentCombo)
-        {
-            if (indexInCurrentCombo == 0)
-                return convertImpactCircle(obj, index);
-            else
-                return convertDefaultCircle(obj, index, indexInCurrentCombo);
-        }
-
-        private static List<TouhosuHitObject> convertDefaultCircle(HitObject obj, int index, int indexInCurrentCombo)
-        {
-            List<TouhosuHitObject> hitObjects = new List<TouhosuHitObject>();
-
-            var circlePosition = (obj as IHasPosition)?.Position ?? Vector2.Zero;
-            circlePosition *= new Vector2(TouhosuPlayfield.X_SCALE_MULTIPLIER, 0.5f);
-            var comboData = obj as IHasCombo;
-
-            hitObjects.AddRange(generateExplosion(
-                obj.StartTime,
-                bullets_per_hitcircle,
-                circlePosition,
-                comboData,
-                index,
-                hitcircle_angle_offset * indexInCurrentCombo));
-
-            hitObjects.Add(new SoundHitObject
-            {
-                StartTime = obj.StartTime,
-                Samples = obj.Samples,
-                Position = circlePosition
-            });
-
-            return hitObjects;
-        }
-
-        private static List<TouhosuHitObject> convertImpactCircle(HitObject obj, int index)
-        {
-            List<TouhosuHitObject> hitObjects = new List<TouhosuHitObject>();
-
-            var circlePosition = (obj as IHasPosition)?.Position ?? Vector2.Zero;
-            circlePosition *= new Vector2(TouhosuPlayfield.X_SCALE_MULTIPLIER, 0.5f);
-            var comboData = obj as IHasCombo;
-
-            var randomBool = MathExtensions.GetRandomTimedBool(obj.StartTime);
-
-            hitObjects.AddRange(generatePolygonExplosion(
-                obj.StartTime,
-                3,
-                randomBool ? 3 : 4,
-                circlePosition,
-                comboData,
-                index,
-                MathExtensions.GetRandomTimedAngleOffset(obj.StartTime)));
-
-            hitObjects.Add(new SoundHitObject
-            {
-                StartTime = obj.StartTime,
-                Samples = obj.Samples,
-                Position = circlePosition
-            });
-
-            return hitObjects;
         }
 
         private static List<TouhosuHitObject> generateDefaultSlider(HitObject obj, IBeatmap beatmap, IHasPathWithRepeats curve, double spanDuration, int index)
@@ -362,29 +292,6 @@ namespace osu.Game.Rulesets.Touhosu.Extensions
             }
         }
 
-        public static List<TouhosuHitObject> ConvertSpinner(HitObject obj, IHasDuration endTime, int index)
-        {
-            List<TouhosuHitObject> hitObjects = new List<TouhosuHitObject>();
-
-            var objPosition = (obj as IHasPosition)?.Position ?? Vector2.Zero;
-            var comboData = obj as IHasCombo;
-
-            var spansPerSpinner = endTime.Duration / spinner_span_delay;
-
-            for (int i = 0; i < spansPerSpinner; i++)
-            {
-                hitObjects.AddRange(generateExplosion(
-                    obj.StartTime + i * spinner_span_delay,
-                    bullets_per_spinner_span,
-                    objPosition * new Vector2(TouhosuPlayfield.X_SCALE_MULTIPLIER, 0.5f),
-                    comboData,
-                    index,
-                    i * spinner_angle_per_span));
-            }
-
-            return hitObjects;
-        }
-
         private static IEnumerable<TouhosuHitObject> generateExplosion(double startTime, int bulletCount, Vector2 position, IHasCombo comboData, int index, float angleOffset = 0, float angleRange = 360f, double? timePreempt = null)
         {
             for (int i = 0; i < bulletCount; i++)
@@ -398,43 +305,6 @@ namespace osu.Game.Rulesets.Touhosu.Extensions
                     ComboOffset = comboData?.ComboOffset ?? 0,
                     IndexInBeatmap = index,
                     CustomTimePreempt = timePreempt
-                };
-            }
-        }
-
-        private static List<TouhosuHitObject> generatePolygonExplosion(double startTime, int bullets_per_side, int verticesCount, Vector2 position, IHasCombo comboData, int index, float angleOffset = 0)
-        {
-            List<TouhosuHitObject> hitObjects = new List<TouhosuHitObject>();
-
-            for (int i = 0; i < verticesCount; i++)
-                hitObjects.AddRange(generatePolygonLine(startTime, bullets_per_side, verticesCount, position, comboData, index, i * (360f / verticesCount) + angleOffset));
-
-            return hitObjects;
-        }
-
-        private static IEnumerable<TouhosuHitObject> generatePolygonLine(double startTime, int bullets_per_side, int verticesCount, Vector2 position, IHasCombo comboData, int index, float additionalOffset = 0)
-        {
-            var s = 1.0;
-            var side = s / (2 * Math.Sin(360.0 / (2 * verticesCount) * Math.PI / 180));
-            var partDistance = s / bullets_per_side;
-            var partialAngle = 180 * (verticesCount - 2) / (2 * verticesCount);
-
-            for (int i = 0; i < bullets_per_side; i++)
-            {
-                var c = (float)partDistance * i;
-                var length = Math.Sqrt(MathExtensions.Pow((float)side) + MathExtensions.Pow(c) - (2 * side * c * Math.Cos(partialAngle * Math.PI / 180)));
-                var missingAngle = c == 0 ? 0 : Math.Acos((MathExtensions.Pow((float)side) + MathExtensions.Pow((float)length) - MathExtensions.Pow(c)) / (2 * side * length)) * 180 / Math.PI;
-                var currentAngle = 180 + (90 - partialAngle) - missingAngle;
-
-                yield return new AngeledProjectile
-                {
-                    Angle = (float)currentAngle + additionalOffset,
-                    DeltaMultiplier = length / side * 1.2f,
-                    StartTime = startTime,
-                    Position = position,
-                    NewCombo = comboData?.NewCombo ?? false,
-                    ComboOffset = comboData?.ComboOffset ?? 0,
-                    IndexInBeatmap = index
                 };
             }
         }
