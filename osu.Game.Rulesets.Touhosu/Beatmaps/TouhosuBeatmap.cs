c0fb8e34c192615207994693e8d7ba84fa4c40b8
Andrei Zavatski <megaman9919@gmail.com>
2020-08-10 22:24:42 +0300

    Update beatmap statistics

--- osu.Game.Rulesets.Touhosu/Beatmaps/TouhosuBeatmap.cs
+++ osu.Game.Rulesets.Touhosu/Beatmaps/TouhosuBeatmap.cs
@@ -12,6 +12,8 @@ namespace osu.Game.Rulesets.Touhosu.Beatmaps
         {
             var explosions = HitObjects.Count(s => s is Explosion);
             var spinners = HitObjects.Count(s => s is Spinner);
+            var homing = HitObjects.Count(s => s is HomingProjectile);
+            var projectiles = HitObjects.Count(s => s is AngeledProjectile);
 
             return new[]
             {
@@ -25,6 +27,18 @@ namespace osu.Game.Rulesets.Touhosu.Beatmaps
                 {
                     Name = @"Spinners",
                     Content = spinners.ToString(),
+                    Icon = FontAwesome.Regular.Circle
+                },
+                new BeatmapStatistic
+                {
+                    Name = @"Projectiles",
+                    Content = projectiles.ToString(),
+                    Icon = FontAwesome.Regular.Circle
+                },
+                new BeatmapStatistic
+                {
+                    Name = @"Homing projectiles",
+                    Content = homing.ToString(),
                     Icon = FontAwesome.Regular.Circle
                 }
             };
