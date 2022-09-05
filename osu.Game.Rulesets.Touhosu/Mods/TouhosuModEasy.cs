83f529dc93f94e31ceb5678ba6fbe26bea75db95
Daniel Thirion <defvs.daniel@gmail.com>
2022-09-05 19:44:10 +0200

    Fix breaking changes in mods Description by using LocalisableString

--- osu.Game.Rulesets.Touhosu/Mods/TouhosuModEasy.cs
+++ osu.Game.Rulesets.Touhosu/Mods/TouhosuModEasy.cs
@@ -1,9 +1,10 @@
-﻿using osu.Game.Rulesets.Mods;
+﻿using osu.Framework.Localisation;
+using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Touhosu.Mods
 {
     public class TouhosuModEasy : ModEasy
     {
-        public override string Description => @"Smaller bullets, more forgiving HP drain and three lives!";
+        public override LocalisableString Description => @"Smaller bullets, more forgiving HP drain and three lives!";
     }
 }
--- osu.Game.Rulesets.Touhosu/Mods/TouhosuModHidden.cs
+++ osu.Game.Rulesets.Touhosu/Mods/TouhosuModHidden.cs
@@ -1,4 +1,5 @@
-﻿using osu.Game.Rulesets.Touhosu.Objects.Drawables;
+﻿using osu.Framework.Localisation;
+using osu.Game.Rulesets.Touhosu.Objects.Drawables;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 
@@ -8,7 +9,7 @@ namespace osu.Game.Rulesets.Touhosu.Mods
     {
         public override double ScoreMultiplier => 1.06;
 
-        public override string Description => "Bullets will become invisible near you.";
+        public override LocalisableString Description => "Bullets will become invisible near you.";
 
         public override void ApplyToDrawableHitObject(DrawableHitObject dho)
         {
