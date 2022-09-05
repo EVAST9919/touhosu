3e9d45fbb681ee6f46a7f31d7842fa85fb890d47
Daniel Thirion <defvs.daniel@gmail.com>
2022-09-05 23:40:57 +0200

    Add NoRegen / DamageMultiplier incompatibility to SuddenDeath

--- osu.Game.Rulesets.Touhosu/Mods/TouhosuModSuddenDeath.cs
+++ osu.Game.Rulesets.Touhosu/Mods/TouhosuModSuddenDeath.cs
@@ -1,8 +1,10 @@
-﻿using osu.Game.Rulesets.Mods;
+﻿using System;
+using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Touhosu.Mods
 {
     public class TouhosuModSuddenDeath : ModSuddenDeath
     {
+        public override Type[] IncompatibleMods => new[] { typeof(TouhosuModNoRegen), typeof(TouhosuModDamageMultiplier) };
     }
 }
